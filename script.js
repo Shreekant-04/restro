343ace46567b2bd1aca79b9df624565382214d8e
Shreekant <shree.kant.74.sk@gmail.com>
2024-07-07 23:00:17 +0530

    css edited

--- script.js
+++ script.js
@@ -12,7 +12,6 @@ document.getElementById("nav-toggle").addEventListener("click", function () {
   navMenu.classList.toggle("show-menu");
 });
 
-
 const menuItems = [
   {
     id: "1",
@@ -81,91 +80,104 @@ const menuItems = [
   {
     id: "9",
     itemName: "Aloo Paratha",
-    image: "https://t3.ftcdn.net/jpg/01/15/62/02/360_F_115620221_5XL4MzVoXrHGJdhHXrVVNL0WUHdJLCPH.jpg",
+    image:
+      "https://t3.ftcdn.net/jpg/01/15/62/02/360_F_115620221_5XL4MzVoXrHGJdhHXrVVNL0WUHdJLCPH.jpg",
     price: "₹120",
     category: "Veg",
   },
   {
     id: "10",
     itemName: "Fish Curry",
-    image: "https://t4.ftcdn.net/jpg/02/96/12/15/360_F_296121509_0Wapf8gyoLCOxk9KjtTVqgASjEXsBDn1.jpg",
+    image:
+      "https://t4.ftcdn.net/jpg/02/96/12/15/360_F_296121509_0Wapf8gyoLCOxk9KjtTVqgASjEXsBDn1.jpg",
     price: "₹300",
     category: "Non-Veg",
   },
   {
     id: "11",
     itemName: "Dal Makhani",
-    image: "https://t4.ftcdn.net/jpg/01/84/58/83/360_F_184588398_RczrKKQfy5GZWSd1rvCYu6GEPlsCd9rv.jpg",
+    image:
+      "https://t4.ftcdn.net/jpg/01/84/58/83/360_F_184588398_RczrKKQfy5GZWSd1rvCYu6GEPlsCd9rv.jpg",
     price: "₹180",
     category: "Veg",
   },
   {
     id: "12",
     itemName: "Prawn Masala",
-    image: "https://t4.ftcdn.net/jpg/04/29/57/71/360_F_429577181_C6LC59Bvrwn0WJPR6GNFDpecwIDjVTWX.jpg",
+    image:
+      "https://t4.ftcdn.net/jpg/04/29/57/71/360_F_429577181_C6LC59Bvrwn0WJPR6GNFDpecwIDjVTWX.jpg",
     price: "₹400",
     category: "Non-Veg",
   },
   {
     id: "13",
     itemName: "Chole Bhature",
-    image: "https://t4.ftcdn.net/jpg/05/97/41/05/360_F_597410510_aVWxjkToHzErUYm3Ammrvm4od2Fjxl1w.jpg",
+    image:
+      "https://t4.ftcdn.net/jpg/05/97/41/05/360_F_597410510_aVWxjkToHzErUYm3Ammrvm4od2Fjxl1w.jpg",
     price: "₹180",
     category: "Veg",
   },
   {
     id: "14",
     itemName: "Keema Naan",
-    image: "https://media-cdn.tripadvisor.com/media/photo-s/17/98/97/1b/keema-naan.jpg",
+    image:
+      "https://media-cdn.tripadvisor.com/media/photo-s/17/98/97/1b/keema-naan.jpg",
     price: "₹250",
     category: "Non-Veg",
   },
   {
     id: "15",
     itemName: "Palak Paneer",
-    image: "https://t4.ftcdn.net/jpg/04/12/57/33/360_F_412573389_1Jp8J0hA4fc1VjxR8D2SPQbdQpTfYkJP.jpg",
+    image:
+      "https://t4.ftcdn.net/jpg/04/12/57/33/360_F_412573389_1Jp8J0hA4fc1VjxR8D2SPQbdQpTfYkJP.jpg",
     price: "₹220",
     category: "Veg",
   },
   {
     id: "16",
     itemName: "Egg Curry",
-    image: "https://t4.ftcdn.net/jpg/05/45/04/09/360_F_545040926_Fd7TY5kXcLuBHyJRAXaajrv949qVB3GC.jpg",
+    image:
+      "https://t4.ftcdn.net/jpg/05/45/04/09/360_F_545040926_Fd7TY5kXcLuBHyJRAXaajrv949qVB3GC.jpg",
     price: "₹150",
     category: "Non-Veg",
   },
   {
     id: "17",
     itemName: "Rajma Chawal",
-    image: "https://t3.ftcdn.net/jpg/07/28/71/10/360_F_728711034_H1fTB6GI74c1icXZY1NBODNqXAky5IO8.jpg",
+    image:
+      "https://t3.ftcdn.net/jpg/07/28/71/10/360_F_728711034_H1fTB6GI74c1icXZY1NBODNqXAky5IO8.jpg",
     price: "₹200",
     category: "Veg",
   },
   {
     id: "18",
     itemName: "Chicken Tikka",
-    image: "https://t3.ftcdn.net/jpg/05/85/17/02/360_F_585170258_aBJpRchNLmMrJaj7vnm3sCd2XPF7jCi0.jpg",
+    image:
+      "https://t3.ftcdn.net/jpg/05/85/17/02/360_F_585170258_aBJpRchNLmMrJaj7vnm3sCd2XPF7jCi0.jpg",
     price: "₹300",
     category: "Non-Veg",
   },
   {
     id: "19",
     itemName: "Veg Hakka Noodles",
-    image: "https://as1.ftcdn.net/v2/jpg/03/86/85/64/1000_F_386856433_UTNRrviaDxUsLJfEpVp24bdr3xqgQXjl.jpg",
+    image:
+      "https://as1.ftcdn.net/v2/jpg/03/86/85/64/1000_F_386856433_UTNRrviaDxUsLJfEpVp24bdr3xqgQXjl.jpg",
     price: "₹180",
     category: "Veg",
   },
   {
     id: "20",
     itemName: "Mango Lassi",
-    image: "https://t4.ftcdn.net/jpg/03/26/17/71/360_F_326177134_sPYEaYBMKsScGsOGbV45FAjaTZueL093.jpg",
+    image:
+      "https://t4.ftcdn.net/jpg/03/26/17/71/360_F_326177134_sPYEaYBMKsScGsOGbV45FAjaTZueL093.jpg",
     price: "₹80",
     category: "Veg",
   },
   {
     id: "21",
     itemName: "Special Thali",
-    image: "https://media.istockphoto.com/id/1158578874/photo/indian-hindu-veg-thali-food-platter-selective-focus.jpg?s=612x612&w=0&k=20&c=ZHAsJ9sJJoeAmwD3iU1Oo2YSKn_BG6JoE7zuG1frxwg=",
+    image:
+      "https://media.istockphoto.com/id/1158578874/photo/indian-hindu-veg-thali-food-platter-selective-focus.jpg?s=612x612&w=0&k=20&c=ZHAsJ9sJJoeAmwD3iU1Oo2YSKn_BG6JoE7zuG1frxwg=",
     price: "₹140",
     category: "Veg",
   },
